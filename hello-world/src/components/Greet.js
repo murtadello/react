cf7fb7a8666462507ceeb528c48b3d33bca938e2
Murtaza <murtaza.mohammadi@powercoders.org>
2019-11-15 16:47:55 +0100

    added the react project

---
+++ hello-world/src/components/Greet.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Greet()
+{
+    return <h1>Hello Murtaza Mohammadi from Afghanistan</h1>
+}
+
+
+ const Message = () => <h1>Hello Murtaza Mohammadi</h1>
+ export default Message 
